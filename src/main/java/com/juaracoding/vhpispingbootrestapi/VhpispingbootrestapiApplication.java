6d6aa9e1e5c5246f37c81cae859e6be64045c3c0
virdhahpi <hanivavirdha@gmail.com>
2023-11-23 22:15:44 +0700

    Praktikum Springboot Rest API

--- src/main/java/com/juaracoding/vhpispingbootrestapi/VhpispingbootrestapiApplication.java
+++ src/main/java/com/juaracoding/vhpispingbootrestapi/VhpispingbootrestapiApplication.java
@@ -1,4 +1,13 @@
 package com.juaracoding.vhpispingbootrestapi;
+/*
+Created by IntelliJ IDEA 2022.2.3 (Community Edition)
+Build #IC-232.10072.27, built on October 11, 2023
+@Author LENOVO a.k.a. Virdha Haniva Pratiwie Ishak
+Java Developer
+Created on 23/11/2023 19:12
+@Last Modified 23/11/2023 19:12
+Version 1.0
+*/
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/juaracoding/vhpispingbootrestapi/controller/CobaController.java
+++ src/main/java/com/juaracoding/vhpispingbootrestapi/controller/CobaController.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.http.HttpStatus;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/main")
 public class CobaController {
     @GetMapping("/v1/welcome")
     public String welcome(){
+        System.out.println("Masuk Kesini");
         return "Hello World!!";
     }
     @GetMapping("/v1/save")
@@ -25,6 +30,13 @@ public class CobaController {
     public String delete(){
         System.out.println("Delete Disini");
         return "Sudah Terhapus";
+    }
+    @GetMapping("/v1/contohdata")
+    public Map<String,Object> contohData(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("nama","virdha");
+        map.put("alamat","sidoarjo");
+        return map;
     }
 
 
